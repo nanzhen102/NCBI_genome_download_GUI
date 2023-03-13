761d0c1b76d2186874401432c5fbea790394ef4a
Nanzhen Qiao <nanzhen.qiao@gmail.com>
2023-03-13 10:18:17 -0600

    Update main.py

--- 3_family_type_update/main.py
+++ 3_family_type_update/main.py
@@ -2,3 +2,16 @@
 # Aim: obtain the latest info about Lactobacillaceae from NCBI
 # Written by: Nanzhen   nanzhen.qiao@gmail.com
 
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.ncbi.nlm.nih.gov/data-hub/taxonomy/tree/?taxon=33958'
+response = requests.get(url)
+
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# inspect the HTML straucture of the web, find the HTML tags to search for
+# a `<a>' tag with 'class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineHover css-1g2ra9k"'
+genus_name = soup.find('a', {'class': 'MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineHover css-1g2ra9k'})
+
+print(genus_name)
